ecfd86c9f0c1d4f737be8d33d3597c91e7df1662
jykhli@gmail.com <jykhli@gmail.com>
2019-04-07 22:01:19 +0200

    ready to deploy

---
+++ build/precache-manifest.9429547125af66411fdbfcfe0d2e5ab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3690fd75864712c8593c",
+    "url": "/static/css/main.f5ae5781.chunk.css"
+  },
+  {
+    "revision": "3690fd75864712c8593c",
+    "url": "/static/js/main.180e2813.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54aa420b7f7ec5391bbb",
+    "url": "/static/js/2.20b4bd11.chunk.js"
+  },
+  {
+    "revision": "03e277cf89a5baa612a8ee4a442c3ff9",
+    "url": "/index.html"
+  }
+];
